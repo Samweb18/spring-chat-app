e7cd50942d1098e6bb87011cfb77c1eae458fae3
sambhav <sambhavkushwaha12@gmail.com>
2026-05-29 04:44:10 +0530

    Add CORS config

--- chat-app-main/chat-app-backend/src/main/java/com/substring/chat/config/WebSocketConfig.java
+++ chat-app-main/chat-app-backend/src/main/java/com/substring/chat/config/WebSocketConfig.java
@@ -29,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")// connection establishment
-                .setAllowedOrigins("https://spring-chat-app.vercel.app")
+                .setAllowedOrigins("https://spring-chat-6vbrxvjvd-sambhavkushwaha12-35255-projects.vercel.app")
                 .withSockJS();
     }
 }
@@ -43,7 +43,9 @@ class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://spring-chat-app.vercel.app")
+                        .allowedOrigins(
+                            "https://spring-chat-6vbrxvjvd-sambhavkushwaha12-35255-projects.vercel.app"
+                        )
                         .allowedMethods("*");
             }
         };
