7fd600ee7f8605fc9476b54259f95ef6a1b4e0da
sambhav <sambhavkushwaha12@gmail.com>
2026-05-29 04:33:57 +0530

    added vercel cors configuration

--- chat-app-main/chat-app-backend/src/main/java/com/substring/chat/config/WebSocketConfig.java
+++ chat-app-main/chat-app-backend/src/main/java/com/substring/chat/config/WebSocketConfig.java
@@ -1,8 +1,11 @@
 package com.substring.chat.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -26,8 +29,23 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")// connection establishment
-                .setAllowedOrigins("http://localhost:5173")
+                .setAllowedOrigins("https://spring-chat-app.vercel.app")
                 .withSockJS();
     }
-    // /chat endpoint par connection apka establish hoga
+}
+
+@Configuration
+class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://spring-chat-app.vercel.app")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
