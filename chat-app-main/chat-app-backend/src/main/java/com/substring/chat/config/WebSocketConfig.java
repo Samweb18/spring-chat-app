4c2ee51c2af3f3a6fc04237e66a1e40588e92543
sambhav <sambhavkushwaha12@gmail.com>
2026-05-29 02:20:45 +0530

    initial commit

---
+++ chat-app-main/chat-app-backend/src/main/java/com/substring/chat/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.substring.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
+
+        config.enableSimpleBroker("/topic");
+        // /topic/messages
+
+        config.setApplicationDestinationPrefixes("/app");
+        // /app/chat
+        // server-side: @MessagingMapping("/chat)
+
+    }
+
+    @Override
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")// connection establishment
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
+    }
+    // /chat endpoint par connection apka establish hoga
+}
